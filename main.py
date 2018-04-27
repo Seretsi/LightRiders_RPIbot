0b74c4864daf70e84416bea5ea38470d5eb6885d
root <jordan.daddeo@gmail.com>
2018-04-26 21:30:27 -0400

    Added tensorflow checker to test dependencies

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import sys
 
 from Bot.game import Game
 from Bot.bot import Bot
+import tensorflow as tf
 
 def main():
     bot = Bot()
     game = Game()
     game.run(bot)
+    hello = tf.constant('Hello, TensorFlow!')
+    sess = tf.Session() 
+    print(sess.run(hello))
 
 if __name__ == '__main__':
     main()
